f11c8563579b6eab902b8fadcb4783c1cdde980e
astrobounce <kmrarv@gmail.com>
2019-09-15 00:01:05 +0530

    Added comments

--- chapter-3/hello_world.go
+++ chapter-3/hello_world.go
@@ -1,7 +1,14 @@
 package main
 
+// Import Required Packages
+
 import "fmt"
 
+// Main function - Entrypoint of Program
+
 func main() {
+	/* 
+	 Printing Hello World using Prinltn
+	*/
 	fmt.Println("Hello World")
 }
